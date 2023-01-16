4e1650b1259ab1ff10e7bf0cad50d11b0071178d
Peterdinis <peterdinis0@pobox.sk>
2023-01-16 16:14:21 +0100

    feat: Start to updating application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,11 @@
 import Layout from '@/components/shared/Layout';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import {
-  RecoilRoot,
-} from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
       <Layout>
       <Component {...pageProps} />
       </Layout>
-    </RecoilRoot>
   )
 }
