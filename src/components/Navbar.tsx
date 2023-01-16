01650ecdfcf4c1a205efa3a84637399f86fc060f
Peterdinis <peterdinis0@pobox.sk>
2023-01-16 15:16:30 +0100

    feat: Updating sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar: React.FC = () => {
     return (
         <>
-        gggg
+      
         </>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import Sidebar from "@/components/sidebars/Sidebar";
+
 export default function Home() {
   return (
     <>
-      PING
+      <Sidebar />
     </>
   )
 }
