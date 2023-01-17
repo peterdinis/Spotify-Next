6996c6514ec91d2c66ca4949852f6800845a03fc
Peterdinis <peterdinis0@pobox.sk>
2023-01-17 18:09:43 +0100

    feat: New components and pages

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -1,9 +1,19 @@
-const Navbar: React.FC = () => {
-    return (
-        <>
-         ofof
-        </>
-    )
-}
+import Icon from "../Icons";
+import { useRouter } from "next/router";
 
-export default Navbar
+const Navbar: React.FC = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <button className="bg-brenk p-[6px] rounded-full items-center mr-3">
+        <Icon name="back" />
+      </button>
+      <button className="bg-brenk p-[6px] rounded-full items-center mr-2">
+        <Icon name="next" />
+      </button>
+    </>
+  );
+};
+
+export default Navbar;
