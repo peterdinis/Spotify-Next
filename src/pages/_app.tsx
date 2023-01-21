a3a409f06c02c0e20824c4de3c2a2ddbb28742a1
Peterdinis <peterdinis0@pobox.sk>
2023-01-21 20:51:10 +0100

    feat: Update _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from "@/components/shared/Layout";
+import reducer, { initialState } from "@/redux/reducer";
+import { StateProvider } from "@/redux/state";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <Layout>
+        <StateProvider initialState={initialState} reducer={reducer}>
         <Component {...pageProps} />
+        </StateProvider>
       </Layout>
   );
 }
