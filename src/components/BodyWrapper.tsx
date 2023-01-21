accd054f44890f634b189be406be1a00ca342c90
Peterdinis <peterdinis0@pobox.sk>
2023-01-21 21:59:08 +0100

    fix: FOO

--- src/components/BodyWrapper.tsx
+++ src/components/BodyWrapper.tsx
@@ -1,9 +1,75 @@
-const BodyWrapper: React.FC = () => {
-    return (
-        <>
-            fkfkfk
-        </>
-    )
-}
+import { useEffect, useRef, useState } from "react";
+import Sidebar from "./shared/Sidebar";
+import Footer from "./shared/Footer";
+import Navbar from "./shared/Navbar";
+import axios from "axios";
+import { useStateProvider } from "@/redux/state";
+import Wrapper from "./shared/Wrapper";
+import { spotifyConstants } from "@/redux/constants";
+import { HomeContainer } from "@/styles/Component.styled";
 
-export default BodyWrapper
+const BodyWrapper: React.FC = () => {
+  const [{ token }, dispatch] = useStateProvider();
+  const [navBackground, setNavBackground] = useState(false);
+  const [headerBackground, setHeaderBackground] = useState(false);
+  const bodyRef = useRef<any>();
+  const bodyScrolled = () => {
+    bodyRef.current.scrollTop >= 30
+      ? setNavBackground(true)
+      : setNavBackground(false);
+    bodyRef.current.scrollTop >= 268
+      ? setHeaderBackground(true)
+      : setHeaderBackground(false);
+  };
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const { data } = await axios.get("https://api.spotify.com/v1/me", {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+      const userInfo = {
+        userId: data.id,
+        userUrl: data.external_urls.spotify,
+        name: data.display_name,
+      };
+      dispatch({ type: spotifyConstants.SET_USER, userInfo });
+    };
+    getUserInfo();
+  }, [dispatch, token]);
+  useEffect(() => {
+    const getPlaybackState = async () => {
+      const { data } = await axios.get("https://api.spotify.com/v1/me/player", {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+      dispatch({
+        type: spotifyConstants.SET_PLAYER_STATE,
+        playerState: data.is_playing,
+      });
+    };
+    getPlaybackState();
+  }, [dispatch, token]);
+
+  return (
+    <HomeContainer>
+      <div className="spotify__body">
+        <Sidebar />
+        <div className="body" ref={bodyRef} onScroll={bodyScrolled}>
+          <Navbar />
+          <div className="body__contents">
+            <Wrapper headerBackgroundColor={""} />
+          </div>
+        </div>
+      </div>
+      <div className="spotify__footer">
+        <Footer />
+      </div>
+    </HomeContainer>
+  );
+};
+
+export default BodyWrapper;
