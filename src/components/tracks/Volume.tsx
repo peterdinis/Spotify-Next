6611a098cf5d160c6c2b92d633198494b93122dc
Peterdinis <peterdinis0@pobox.sk>
2023-01-21 20:50:18 +0100

    FOO

--- src/components/tracks/Volume.tsx
+++ src/components/tracks/Volume.tsx
@@ -1,11 +1,29 @@
 import { VolumeContainer } from "@/styles/Component.styled";
 import axios from "axios";
+import { useStateProvider } from "@/redux/state";
 
 const Volume: React.FC = () => {
+  const [{ token }] = useStateProvider();
+  const setVolume = async (e: any) => {
+    await axios.put(
+      "https://api.spotify.com/v1/me/player/volume",
+      {},
+      {
+        params: {
+          volume_percent: parseInt(e.target.value),
+        },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + process.env.NEXT_PUBLIC_CLIENT_ID as string,
+        },
+      }
+    );
+  };
+  
   return (
     <>
       <VolumeContainer>
-        <input type="range" />
+      <input type="range" onMouseUp={(e) => setVolume(e)} min={0} max={100} />
       </VolumeContainer>
     </>
   );
