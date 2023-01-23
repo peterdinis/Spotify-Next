6c2493d7ad9c3221a10987d274f5db13a4f3a59a
Peterdinis <peterdinis0@pobox.sk>
2023-01-23 11:59:00 +0100

    feat: Updating reducer

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -2,7 +2,16 @@ import { InitialStateI, ReduxAction} from "@/typing/contextTypes";
 
 
 const reducer = (state: InitialStateI, action: ReduxAction) => {
-    
+    switch(action.type) {
+        case "SET_USER":
+            return {
+                ...state,
+                user: action.user,
+            }
+
+        default:
+            return state
+    }
 }
 
 export default reducer;
